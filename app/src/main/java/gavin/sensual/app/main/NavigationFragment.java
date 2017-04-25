78e9552e57922f1971604dd3ffd0470fa91b8201
gavinxxxxxx <gavinxxxxxx@gmail.com>
2017-04-25 18:11:54 +0800

    侧滑菜单自定义样式

--- app/src/main/java/gavin/sensual/app/main/NavigationFragment.java
+++ app/src/main/java/gavin/sensual/app/main/NavigationFragment.java
@@ -2,6 +2,8 @@ package gavin.sensual.app.main;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.SwitchCompat;
+import android.widget.TextView;
 
 import gavin.sensual.R;
 import gavin.sensual.base.BaseFragment;
@@ -29,5 +31,15 @@ public class NavigationFragment extends BaseFragment<FragNavigationBinding> {
         if (savedInstanceState == null) {
             loadRootFragment(R.id.holder, ScrollingFragment.newInstance());
         }
+        init();
+    }
+
+    private void init() {
+        TextView textView = (TextView) binding.navigation.getMenu().findItem(R.id.nav_tip).getActionView();
+        textView.setText("9+");
+        TextView textView2 = (TextView) binding.navigation.getMenu().findItem(R.id.nav_msg).getActionView().findViewById(R.id.textView);
+        textView2.setText("提示信息");
+        SwitchCompat switchCompat = (SwitchCompat) binding.navigation.getMenu().findItem(R.id.nav_switch).getActionView();
+        switchCompat.setChecked(true);
     }
 }
